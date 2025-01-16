cc381abc6c7ef07f8156629aba6cb29701eb2d62
Saynez667 <saynezironx@gmail.com>
2025-01-16 11:43:49 +0100

    i have done task 1 and pycodestyle for task 0

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-positive = (number>0)
-zero = (number==0)
-negative = (number<0)
+positive = (number > 0)
+zero = (number == 0)
+negative = (number < 0)
 if positive:
-	print(f"{number} is positive")
+    print(f"{number} is positive")
 if zero:
-	print(f"{number} is zero")
+    print(f"{number} is zero")
 if negative:
-	print(f"{number} is negative")
+    print(f"{number} is negative")
