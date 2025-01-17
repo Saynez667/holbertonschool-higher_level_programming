8e979aa79bd1bfc8837f0b5de120b43acbfbf266
Saynez667 <saynezironx@gmail.com>
2025-01-17 18:50:11 +0100

    i have remove temporaly files

--- python-import_modules/4-hidden_discovery.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import hidden_4
-
-if __name__ == "__main__":
-    names = dir(hidden_4)
-    for name in sorted(names):
-        if not name.startswith("__"):
-            print(name)
