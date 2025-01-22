3231e1cd1040d3b5177049c12a18152b35607821
Saynez667 <saynezironx@gmail.com>
2025-01-22 21:17:01 +0100

    i resole some probleme with task 7 and 5 and pycodestyle for 9

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    print("my_list: {}".format(my_list))
--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        tuple_a += (0,)*(2-len(tuple_a))
-    if len(tuple_b) < 2:
-        tuple_b += (0,)*(2-len(tuple_b))
-    a1, a2 = tuple_a[:2]
-    b1, b2 = tuple_b[:2]
-    return (a1 + b1, a2)
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return new_tuple
