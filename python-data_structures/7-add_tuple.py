69c510d85f050ca6a520a43d6ff24ba127f8d1c7
Saynez667 <saynezironx@gmail.com>
2025-01-22 11:58:47 +0100

    i have done task 7,8,9

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a += (0,)*(2-len(tuple_a))
+    if len(tuple_b) < 2:
+        tuple_b += (0,)*(2-len(tuple_b))
+    a1, a2 = tuple_a[:2]
+    b1, b2 = tuple_b[:2]
+    return (a1 + b1, a2)
