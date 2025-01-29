e573e3d17b09a46eaabbd2a71eace30990cf2dba
Saynez667 <saynezironx@gmail.com>
2025-01-29 11:44:26 +0100

    i have done task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+'''
+Private module for classes Square
+'''
+
+
 class Square:
+    '''
+    Defines a simple class representing a square
+    '''
     def __init__(self, size):
-        self.__size = size
+        '''
+        Initializes the size of the square 
+        '''
+        self.__size = size  
