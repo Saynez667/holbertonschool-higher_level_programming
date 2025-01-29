c0e541d044aca3ffc22f4b2b4a1d3582e5527d03
Saynez667 <saynezironx@gmail.com>
2025-01-29 12:41:21 +0100

    i want to test a code for 3

--- python-classes/3-main.py
+++ python-classes/3-main.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
-Square = __import__('3-square').Square
+class Square:
+    """Defines a square"""
 
-my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
+    def __init__(self, size=0):
+        """
+        Initializes the square with an optional size.
+        Args:
+            size (int): The size of the square (default is 0).
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
