d2e564a77b7cda31b1e9f9fc6793165d4ce7c5ba
Saynez667 <saynezironx@gmail.com>
2025-01-31 09:43:19 +0100

    i have done task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,14 +1,19 @@
+#!/usr/bin/python3
 class Rectangle:
+    '''Represents a rectangle'''
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        '''Retrieve the width of the rectangle'''
         return self.__width
 
     @width.setter
     def width(self, value):
+        '''Set the width of the rectangle'''
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -17,22 +22,14 @@ class Rectangle:
 
     @property
     def height(self):
+        '''Retrieve the height of the rectangle'''
         return self.__height
 
     @height.setter
     def height(self, value):
+        '''Set the height of the rectangle'''
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-if __name__ == "__main__":
-    Rectangle = __import__('1-rectangle').Rectangle
-
-    my_rectangle = Rectangle(2, 4)
-    print(my_rectangle.__dict__)
-
-    my_rectangle.width = 10
-    my_rectangle.height = 3
-    print(my_rectangle.__dict__)
