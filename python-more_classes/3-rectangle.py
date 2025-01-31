b14ce756fedbccb6f3fbb02d85323522b81128b4
Saynez667 <saynezironx@gmail.com>
2025-01-31 15:19:35 +0100

    i resolve some probleme with my code

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -58,6 +58,3 @@ class Rectangle:
             return ""
         return "\n".join(["#" * self.width for _ in range(self.height)])
 
-    def __repr__(self):
-        '''Return a string representation of the rectangle'''
-        return f"Rectangle({self.width}, {self.height})"
