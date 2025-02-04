33b23317c6682e481d746018516f98cd83ec0b97
Saynez667 <saynezironx@gmail.com>
2025-02-04 02:26:48 +0100

    i have done task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''Module for def a class with arg obj and a_class'''
+
+
 def is_same_class(obj, a_class):
     '''return  the exact class obj type obj and is a class is same'''
     return type(obj) is a_class
