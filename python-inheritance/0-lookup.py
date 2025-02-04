7a4ace0e4eef06aa6f8a0bcc06c901e890aaabe9
Saynez667 <saynezironx@gmail.com>
2025-02-04 02:24:41 +0100

    i have add some comments in the 1

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Module for create a class with obj'''
+
 
 
 def lookup(obj):
---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def is_same_class(obj, a_class):
+    '''return  the exact class obj type obj and is a class is same'''
+    return type(obj) is a_class
