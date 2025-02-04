eac824ebe74317db49a21f3f38369c83c1daad72
Saynez667 <saynezironx@gmail.com>
2025-02-04 21:45:02 +0100

    i have done task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Module for class obj a_class '''
+
+
+def is_kind_of_class(obj, a_class):
+    '''True if the object is an instance of the class or a class'''
+    return isinstance(obj, a_class)
