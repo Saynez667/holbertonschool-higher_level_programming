2b5b7de81aa4fb28c0d8e2da3a0a58fb606eaf83
Saynez667 <saynezironx@gmail.com>
2025-02-05 11:57:46 +0100

    i have add some comments for the 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -6,11 +6,14 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-
+    '''square class that inherits from rectangle'''
     def __init__(self, size):
+        '''Initialize size as private attribut and validate'''
         self.__size = size
         self.integer_validator("size", size)
+        '''super delegue heigth et width a size'''
         super().__init__(size, size)
 
     def area(self):
+        '''return the area of the square'''
         return self.__size ** 2
