b482bc23f8b43e9fe5b5581b17cdc315a0ba53df
Saynez667 <saynezironx@gmail.com>
2025-02-06 15:18:29 +0100

    i have add some comments for task 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,13 +1,8 @@
-#!/usr/bin/env python3
-'''Module for shapes using abstract base class'''
-
 from abc import ABC, abstractmethod
 import math
 
 
 class Shape(ABC):
-    '''Abstract base class for shapes'''
-
     @abstractmethod
     def area(self):
         pass
@@ -18,37 +13,40 @@ class Shape(ABC):
 
 
 class Circle(Shape):
-    '''initialise radius in private class atribut'''
     def __init__(self, radius):
-        self.__radius = radius
+        self.radius = radius
 
-    '''return for area pi*raidius**2'''
     def area(self):
-        return math.pi * self.__radius ** 2
+        return math.pi * self.radius ** 2
 
-    '''return for perimeter 2*pi * radius'''
     def perimeter(self):
-        return 2 * math.pi * self.__radius
+
+        return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
-    '''initialise for private class atribut width and height'''
-    def __init__(self, height, width):
-        self.__height = height
-        self.__width = width
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
-    '''for area height * width'''
     def area(self):
-        return self.__width * self.__height
+        return self.width * self.height
 
-    '''for perimeter return 2* width + height'''
     def perimeter(self):
-        return 2 * self.__width + self.__height
+        return 2 * (self.width + self.height)
 
 
-def shape_info(shape: Shape):
-    '''Function to display the area and perimeter of a shape'''
-    shape_perimeter = shape.perimeter()
-    shape_area = shape.area()
-    print(f"Area: {shape_area}")
-    print(f"Perimeter: {shape_perimeter}")
+def shape_info(shape):
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * abs(self.radius) ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * abs(self.radius)
