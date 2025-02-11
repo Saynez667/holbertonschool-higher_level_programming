d10f36c904ceb09e773e3228972ece88ae796a0a
Saynez667 <saynezironx@gmail.com>
2025-02-11 12:19:18 +0100

    i have resolve some probleme with my code

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+import json
 '''module for return JSON'''
 
 
 def to_json_string(my_obj):
     '''return the JSON repr of an object'''
-    return my_obj
+    return json.dumps(my_obj)
