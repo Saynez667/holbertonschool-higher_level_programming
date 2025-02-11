e150ec1e333018cd26382b1c711a75d80ca9edb4
Saynez667 <saynezironx@gmail.com>
2025-02-11 11:33:07 +0100

    i have done task 0

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10, 10)
 positive = (number > 0)
 zero = (number == 0)
---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    """Reads a UTF8 text file and prints its content to stdout."""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read())
