f0567a236965a8fb84493cec287702432d6d68c0
Saynez667 <saynezironx@gmail.com>
2025-02-12 20:24:35 +0100

    i have done task 1 and all comments

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+'''module for pickle'''
+import pickle
+
+
+class CustomObject():
+    '''Class CustomObject to initialize an instance with name, age, and
+    student status'''
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+        if not isinstance(name, str):
+            raise ValueError("The 'name' parameter must be a string")
+
+        if not isinstance(age, int):
+            raise ValueError("The 'age' parameter must be a intergers")
+
+        if not isinstance(is_student, bool):
+            raise ValueError("The 'is_student' parameter must be a boolean")
+
+    def Display(self):
+        '''Display the object's attributes'''
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+
+def serialize(self, filename):
+    '''Serialize the object and save to a file'''
+    with open(filename, "wb") as file:
+        pickle.dump(self, file)
+
+
+@classmethod
+def deserialize(cls, filename):
+    '''Deserialize an object from a file'''
+    with open(filename, "rb") as file:
+        return pickle.load(file)
