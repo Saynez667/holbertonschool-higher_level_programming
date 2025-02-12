fcd556bf163052f0854f41e32d212e7d821284f6
Saynez667 <saynezironx@gmail.com>
2025-02-12 20:27:51 +0100

    i resolve some problem with my code

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -4,21 +4,18 @@ import pickle
 
 
 class CustomObject():
-    '''Class CustomObject to initialize an instance with name, age, and
-    student status'''
+    '''Class CustomObject to initialize an instance with name, age, and stu'''
     def __init__(self, name, age, is_student):
+        if not isinstance(name, str):
+            raise ValueError("The 'name' parameter must be a string")
+        if not isinstance(age, int):
+            raise ValueError("The 'age' parameter must be an integer")
+        if not isinstance(is_student, bool):
+            raise ValueError("The 'is_student' parameter must be a boolean")
+
         self.name = name
         self.age = age
         self.is_student = is_student
-
-        if not isinstance(name, str):
-            raise ValueError("The 'name' parameter must be a string")
-
-        if not isinstance(age, int):
-            raise ValueError("The 'age' parameter must be a intergers")
-
-        if not isinstance(is_student, bool):
-            raise ValueError("The 'is_student' parameter must be a boolean")
 
     def Display(self):
         '''Display the object's attributes'''
@@ -26,20 +23,13 @@ class CustomObject():
         print(f"Age: {self.age}")
         print(f"Is Student: {self.is_student}")
 
-    def serialize(self, filename: str):
-        """Serialize the current instance and save to a file."""
-        try:
-            with open(filename, "wb") as file:
-                pickle.dump(self, file)
-        except (OSError, pickle.PickleError) as e:
-            print(f"Serialization error: {e}")
+    def serialize(self, filename):
+        '''Serialize the object and save to a file'''
+        with open(filename, "wb") as file:
+            pickle.dump(self, file)
 
     @classmethod
-    def deserialize(cls, filename: str):
-        """Load an instance from a file and return it."""
-        try:
-            with open(filename, "rb") as file:
-                return pickle.load(file)
-        except (OSError, pickle.PickleError, EOFError) as e:
-            print(f"Deserialization error: {e}")
-            return None
+    def deserialize(cls, filename):
+        '''Deserialize an object from a file'''
+        with open(filename, "rb") as file:
+            return pickle.load(file)
