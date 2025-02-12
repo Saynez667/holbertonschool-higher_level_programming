660b67855450280f0cdf6a5f25b03ae152d4a61d
Saynez667 <saynezironx@gmail.com>
2025-02-12 08:43:52 +0100

    i have done task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+'''module for create a main'''
 import sys
 
-
+'''import the code from the before task'''
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+'''add a name for the file'''
 filename = "add_item.json"
 
+'''try to load form a json file add item.json expect FileNotFounderError'''
 try:
     items = load_from_json_file(filename)
 except FileNotFoundError:
