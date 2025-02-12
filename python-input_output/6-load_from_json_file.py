20bc82c986c9cf492fd3325accf2da31db95dc15
Saynez667 <saynezironx@gmail.com>
2025-02-12 08:30:36 +0100

    i have done task 6 with comments for create an obj form a json file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     '''function for create an object from JSON FILE'''
-    with open(filename, "r+", encoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         return json.load(file)
