81c232f82686e78134cbca4e1c996b63b68185b0
Saynez667 <saynezironx@gmail.com>
2025-03-05 20:43:49 +0100

    i have done task 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- This scripts prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL 
-SHOW CREATE DATABASE if not exists hbtn_0c_0;
+SHOW CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
