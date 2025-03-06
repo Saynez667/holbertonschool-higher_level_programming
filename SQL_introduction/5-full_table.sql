54b7215eac102b944b9f15ab95b67b85b39e4c2c
Saynez667 <saynezironx@gmail.com>
2025-03-06 05:54:48 +0100

    i want to test a code for task5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
--- This scripts prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL 
-SHOW CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- This scripts prints the following description of the table first_table
+SELECT TABLE_NAME, COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
