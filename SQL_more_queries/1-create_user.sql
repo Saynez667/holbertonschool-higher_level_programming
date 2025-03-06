3a16b71e539953ccb4cd72fbd2278a0c63d70dfa
Saynez667 <saynezironx@gmail.com>
2025-03-06 21:19:01 +0100

    i have done task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges on all databases of your MySQL server to the user user_0d_1.
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
 -- Update the privileges of the MySQL user user_0d_1.	
 FLUSH PRIVILEGES;
