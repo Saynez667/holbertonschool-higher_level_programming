2944ff10da93ddacad981465b9f1922ee75850fc
Saynez667 <saynezironx@gmail.com>
2025-03-06 21:32:18 +0100

    i have done task 4

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- This script that creates the table id_not_null with the following requirements:
 CREATE TABLE IF NOT EXISTS id_not_null (
-	id INT VALUE '1',
+	id INT DEFAULT 1,
 	name VARCHAR(256)
 );
