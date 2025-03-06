4fedb8ed9671931dcd1cf64b687b03c0ad70d3f3
Saynez667 <saynezironx@gmail.com>
2025-03-06 06:01:12 +0100

    i want to test a code for task6

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
--- this scripts lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL
-SHOW rows FROM first_table;
+-- this scripts lists all rows of the table first_table from the database
+SHOW VALUES first_table;
