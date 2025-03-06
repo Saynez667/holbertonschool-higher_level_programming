1865b17b72f545e9e49eb467417f92519ad0df8a
Saynez667 <saynezironx@gmail.com>
2025-03-06 21:18:01 +0100

    i have done task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,10 +1,8 @@
 -- This script that creates the MySQL server user user_0d_1.
-SET GLOBAL validate_password.policy=LOW;
-CREATE USER IF NOT EXISTS
-'user_0d_1'@'localhost'IDENTIFIED BY
-'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges on all databases of your MySQL server to the user user_0d_1.
-GRANT ALL PRIVILEGES ON *.* TO
-'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Update the privileges of the MySQL user user_0d_1.	
 FLUSH PRIVILEGES;
