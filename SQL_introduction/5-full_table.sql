f40c4c070c7eb76f7415017c372045b09f47ac15
Saynez667 <saynezironx@gmail.com>
2025-03-06 05:56:05 +0100

    i want to test a code for task5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- This scripts prints the following description of the table first_table
-SELECT TABLE_NAME, COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
