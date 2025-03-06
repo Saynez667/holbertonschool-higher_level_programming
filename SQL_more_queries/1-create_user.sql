489686f3276be2d3ac426e9429a34ed28c441175
Saynez667 <saynezironx@gmail.com>
2025-03-06 21:16:08 +0100

    i have done task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,6 +3,8 @@ SET GLOBAL validate_password.policy=LOW;
 CREATE USER IF NOT EXISTS
 'user_0d_1'@'localhost'IDENTIFIED BY
 'user_0d_1_pwd';
+
+-- Grant all privileges on all databases of your MySQL server to the user user_0d_1.
 GRANT ALL PRIVILEGES ON *.* TO
 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
