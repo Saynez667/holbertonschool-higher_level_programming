b7c274d476aaacaa9a35c5124dd41b6e0d130913
Saynez667 <saynezironx@gmail.com>
2025-03-06 21:43:48 +0100

    i have done task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- That create the tables cities in the database hbtn_0d_usa with the following requirements:
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
 	name VARCHAR(256) NOT NULL
 );
