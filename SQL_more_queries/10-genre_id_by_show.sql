f6c781e8ba474559576ebba447e636b6102a1e1b
Saynez667 <saynezironx@gmail.com>
2025-03-06 22:08:22 +0100

    i have done task 10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- this script lists all shows and their genre id's, in ascending order by show title and genre id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
