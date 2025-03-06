0040ec40b29b767cbbc19b8eb1d411e8a3534fc4
Saynez667 <saynezironx@gmail.com>
2025-03-06 21:45:41 +0100

    i have done task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,12 +1,13 @@
--- This script that creates the database hbtn_0d_usa
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- Switch to the newly created database
 USE hbtn_0d_usa;
 
--- That create the tables cities in the database hbtn_0d_usa with the following requirements:
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	state_id INT NOT NULL FOREIGN KEY (state_id) REFERENCES states(id),
-	name VARCHAR(256) NOT NULL
+-- Create the cities table
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
