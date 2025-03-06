e397e3a4b9c655e236b58d77d59c979ae302fa68
Saynez667 <saynezironx@gmail.com>
2025-03-06 07:02:06 +0100

    i have done task  16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,5 @@
 -- This script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score, name 
+SELECT score, name
 FROM second_table
+WHERE name IS NOT NULL AND name != ''
 ORDER BY score DESC;
