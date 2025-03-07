b5ef2407b4922ad197bb6d6bb42da2154f3099dc
Saynez667 <saynezironx@gmail.com>
2025-03-07 11:13:15 +0100

    i have done task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,33 +1,46 @@
 #!/usr/bin/python3
-"""Script that lists all cities from the database"""
+"""
+Script that lists all cities from the database `hbtn_0e_4_usa`,
+including the corresponding state name.
+"""
 
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    # Connect to MySQL server
+
+    """ Get arguments from the command line """
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    """ Connect to MySQL database """
     db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        host="localhost",  # MySQL server host
+        port=3306,  # Default MySQL port
+        user=mysql_username,  # MySQL username
+        passwd=mysql_password,  # MySQL password
+        db=database_name  # Database name
     )
 
-    # Create cursor object
-    cursor = db.cursor()
+    """ Create a cursor object to execute SQL queries """
+    cur = db.cursor()
 
-    # Execute query with JOIN to get cities with state names
-    query = """SELECT cities.id, cities.name, states.name
-               FROM cities
-               JOIN states ON cities.state_id = states.id
-               ORDER BY cities.id ASC"""
-    cursor.execute(query)
+    """Execute a query to retrieve cities and their state names, ordered
+    by cities.id"""
+    query = """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC
+    """
+    cur.execute(query)
 
-    # Fetch and print results
-    for row in cursor.fetchall():
+    """ Fetch all results and print each row """
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
 
-    # Close cursor and connection
-    cursor.close()
+    """ Close the cursor and database connection """
+    cur.close()
     db.close()
