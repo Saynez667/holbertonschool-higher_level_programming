ce791a4da90388ee471c0fa024e206c03000a8f3
Saynez667 <saynezironx@gmail.com>
2025-03-07 11:04:19 +0100

    i have done task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
     """ Main execution: Connects to MySQL, queries the
     database safely, and prints results. """
 
-    # Get arguments from the command line
+    """ Get arguments from the command line """
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
     state_name_searched = sys.argv[4]
 
-    # Connect to MySQL database
+    """ Connect to MySQL database """
     db = MySQLdb.connect(
         host="localhost",  # MySQL server host
         port=3306,  # Default MySQL port
@@ -26,18 +26,18 @@ if __name__ == "__main__":
         db=database_name  # Database name
     )
 
-    # Create a cursor object to execute SQL queries
+    """ Create a cursor object to execute SQL queries """
     cur = db.cursor()
 
-    # Execute a parameterized query to prevent SQL injection
+    """ Execute a parameterized query to prevent SQL injection """
     query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"
     cur.execute(query, (state_name_searched,))
 
-    # Fetch all results and print each row
+    """ Fetch all results and print each row """
     rows = cur.fetchall()
     for row in rows:
         print(row)
 
-    # Close the cursor and database connection
+    """ Close the cursor and database connection """
     cur.close()
     db.close()
