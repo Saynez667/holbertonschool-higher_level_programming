0e80e030126462d8adb8bc1991cf147e263dfacc
Saynez667 <saynezironx@gmail.com>
2025-03-07 09:58:22 +0100

    i want to resolve a probleme with task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name starting with N
-from the database hbtn_0e_0_usa
+Script that lists all states with a name starting with N from
+the database hbtn_0e_0_usa
 """
 
 import MySQLdb
@@ -37,6 +37,7 @@ def filter_states():
     # Fermeture du curseur et de la connexion
     cur.close()
     db.close()
+
 
 if __name__ == "__main__":
     filter_states()
