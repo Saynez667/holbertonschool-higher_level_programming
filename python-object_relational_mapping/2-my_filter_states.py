c4bbe1e6e87678c678570923cd1b3856136c991e
Saynez667 <saynezironx@gmail.com>
2025-03-07 10:18:54 +0100

    i have done task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,48 +1,32 @@
 #!/usr/bin/python3
-"""
-Script that takes in an argument and displays all values in the states table
-where name matches the argument (safe from SQL injections).
-"""
+"""Script that lists all states with a name starting with N from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
 
-def filter_states_by_user_input():
-    """Function that filters states by user input"""
-    if len(sys.argv) != 5:
-        return
-
+if __name__ == "__main__":
     # Connect to the database
     db = MySQLdb.connect(
         host="localhost",
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
+        db=sys.argv[3]
     )
 
     # Create a cursor
     cur = db.cursor()
 
-    # Get the state name
-    state_name = sys.argv[4]
+    state_name= sys.argv[4]
 
-    # Excexuter a query
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        state_name
-    )
+    # SQL query to select states starting with 'N'
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY stade.id ASC".format(state_name))
 
-    cur.execute(query)
-
-    # get the results
+    # Fetch and display the results
     for row in cur.fetchall():
         print(row)
 
-    # Close all cursors
+    # Close cursor and database connection
     cur.close()
     db.close()
-
-
-if __name__ == "__main__":
-    filter_states_by_user_input()
