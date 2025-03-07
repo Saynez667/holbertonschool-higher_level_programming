245e2d54c4172fb36d7f49e2ce2c36aab2082760
Saynez667 <saynezironx@gmail.com>
2025-03-07 11:32:44 +0100

    i have done task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,46 +1,52 @@
 #!/usr/bin/python3
 """
-Script that lists all cities from the database `hbtn_0e_4_usa`,
-including the corresponding state name.
+Script that lists all cities from the database hbtn_0e_4_usa
 """
-
-import sys
 import MySQLdb
+import sys
+
+
+def connectDb(user, password, db):
+    """
+        Get connection with the database.
+        Args:
+            user (str): Username of the user.
+            password (str): Password of the user.
+            db (str): Database to retrieve.
+        Return:
+            Connection database.
+    """
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=db,
+        charset="utf8"
+    )
+    return conn
+
 
 if __name__ == "__main__":
+    # Get command line arguments
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
 
-    """ Get arguments from the command line """
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    # Connect to database
+    conn = connectDb(user, password, db)
+    cursor = conn.cursor()
 
-    """ Connect to MySQL database """
-    db = MySQLdb.connect(
-        host="localhost",  # MySQL server host
-        port=3306,  # Default MySQL port
-        user=mysql_username,  # MySQL username
-        passwd=mysql_password,  # MySQL password
-        db=database_name  # Database name
-    )
-
-    """ Create a cursor object to execute SQL queries """
-    cur = db.cursor()
-
-    """Execute a query to retrieve cities and their state names, ordered
-    by cities.id"""
-    query = """
-        SELECT cities.id, cities.name, states.name
-        FROM cities
+    # Query to find all cities with their state name
+    query = """ SELECT cities.id, cities.name, states.name FROM cities
         JOIN states ON cities.state_id = states.id
         ORDER BY cities.id ASC
     """
-    cur.execute(query)
+    cursor.execute(query)
 
-    """ Fetch all results and print each row """
-    rows = cur.fetchall()
+    # Fetch all the results of the query
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
-
-    """ Close the cursor and database connection """
-    cur.close()
-    db.close()
+    cursor.close()
+    conn.close()
